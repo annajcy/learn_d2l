4a96562f9d0c58161aca9adb5e33b966d18db319
jcy <89929647+annajcy@users.noreply.github.com>
2025-11-04 21:03:23 +0800

    impl: cnn

--- d2l/base/function.py
+++ d2l/base/function.py
@@ -1,4 +1,6 @@
+from typing import Tuple
 import torch
+import torch.nn as nn
 
 # softmax and log_sum_exp functions
 def softmax(X: torch.Tensor) -> torch.Tensor:
@@ -31,3 +33,49 @@ def gelu(X: torch.Tensor) -> torch.Tensor:
 
 def swish(X: torch.Tensor, beta: float = 1.0) -> torch.Tensor:
     return X * sigmoid(beta * X)
+
+
+# convolution operation
+def corr2d(X: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1), dtype=X.dtype)
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i:i + h, j:j + w] * K).sum()
+    return Y
+
+def corr2d_multi_in(X: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
+    return sum(corr2d(x, k) for x, k in zip(X, K)) # type: ignore
+
+def corr2d_multi_in_out(X: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
+    return torch.stack([corr2d_multi_in(X, k) for k in K], dim=0)  # type: ignore
+
+def corr2d_multi_in_out_1x1(X: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
+    c_i, h, w = X.shape
+    c_o = K.shape[0]
+    X = X.reshape((c_i, h * w))
+    K = K.reshape((c_o, c_i))
+    Y = torch.matmul(K, X)
+    return Y.reshape((c_o, h, w))
+
+def comp_conv2d(conv2d: nn.Module, X: torch.Tensor) -> torch.Tensor:
+    # X = X.unsqueeze(0).unsqueeze(0)  # Add batch and channel dimensions
+    X = X.reshape((1, 1) + X.shape)
+    Y = conv2d(X)
+    return Y.reshape(Y.shape[2:])  # Remove batch and channel dimensions
+
+def max_pool2d(X: torch.Tensor, pool_size: Tuple[int, int]) -> torch.Tensor:
+    p_h, p_w = pool_size
+    Y = torch.zeros((X.shape[0] - p_h + 1, X.shape[1] - p_w + 1), dtype=X.dtype)
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = X[i: i + p_h, j: j + p_w].max()
+    return Y
+
+def avg_pool2d(X: torch.Tensor, pool_size: Tuple[int, int]) -> torch.Tensor:
+    p_h, p_w = pool_size
+    Y = torch.zeros((X.shape[0] - p_h + 1, X.shape[1] - p_w + 1), dtype=X.dtype)
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = X[i: i + p_h, j: j + p_w].mean()
+    return Y
